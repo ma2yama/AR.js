09d71ffe15767d9f832e6a4f3efabcb3d81d35f0
Matsuyama, Kazuhiro <matsuyama@fusic.co.jp>
2024-12-09 23:17:18 +0900

    reorder imports

--- three.js/src/location-based/index.ts
+++ three.js/src/location-based/index.ts
@@ -1,5 +1,5 @@
+import { DeviceOrientationControls } from './ts/device-orientation-controls';
 import { LocationBased } from './ts/location-based';
 import { WebcamRenderer } from './ts/webcam-renderer';
-import { DeviceOrientationControls } from './ts/device-orientation-controls';
 
 export { LocationBased, WebcamRenderer, DeviceOrientationControls };
